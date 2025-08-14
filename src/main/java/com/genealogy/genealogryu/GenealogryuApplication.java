318982049eac915b44ad1b2b3eecf3bc0af060e0
RYU <rkm0515@gmail.com>
2025-08-14 21:22:44 +0900

    setup

---
+++ src/main/java/com/genealogy/genealogryu/GenealogryuApplication.java
@@ -0,0 +1,13 @@
+package com.genealogy.genealogryu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogryuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenealogryuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genealogy/genealogryu/GenealogryuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genealogy.genealogryu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenealogryuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
